0f796ff5d0b5fb6ff4c5996fe4e02240d8708d92
Aayush Bajirao <aayubaji1208@gmail.com>
2022-02-25 01:13:45 +0530

    Initial commit/Navbar and background image

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from '@material-ui/core';
+
+const navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div className="nav-items">
+        <a href="#">Travelogged</a>
+        <ul className="nav-menu">
+        
+          <li>Pricing</li>
+          <li>Itinerary</li>
+          <li>About Us</li>
+        </ul>
+        </div>
+
+      </nav>
+    </div>
+  );
+};
+
+export default navbar;
